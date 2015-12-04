e22fa54ce6fc11a7f4e0b94842862fca94a95e16
Fankai <475362263@qq.com>
2015-12-04 14:56:41 +0800

    Initial Commit

---
+++ LOL－01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LOL－01
+//
+//  Created by Joker on 15/12/4.
+//  Copyright © 2015年 Joker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LOL－01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LOL－01
+//
+//  Created by Joker on 15/12/4.
+//  Copyright © 2015年 Joker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
